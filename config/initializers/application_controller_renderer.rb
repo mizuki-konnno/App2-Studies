0312c7a567d61320744a310335ea5f52f4576603
mizuki-konnno <m.m.mizuki.0215@gmail.com>
2021-05-12 23:55:46 +0000

    [Finish]3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '494e8e1e3cee74a9fce34e885dc4fe71d03e98001c31f67d6a4de3568556a3864d1ac02cbf85767c9943aeb5cfa11dac7357124bb4a387ab82996fc3e82c74ad'
